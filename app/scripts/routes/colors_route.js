0ff03ae3aef605e663b7407948e5176cbf143bbc
Cameron Chamberlain <hi@camjc.com>
2013-11-09 00:59:30 +1100

    testing query database forbase for single emotion

--- app/scripts/routes/colors_route.js
+++ app/scripts/routes/colors_route.js
@@ -1,6 +1,12 @@
-Emotivecolor.ColorsRoute = Ember.Route.extend({
-  model: function() {
-    return this.get('store').find('color');
-  }
-});
+// Emotivecolor.ColorsRoute = Ember.Route.extend({
+//     model: function() {
+//         return this.get('store').find('color');
+//     }
+// });
 
+
+Emotivecolor.ColorsRoute = Ember.Route.extend({
+    model: function() {
+        return this.get('store').find('color', { emotion: "Angry" });
+    }
+});
