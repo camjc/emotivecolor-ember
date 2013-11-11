53a11987f1bad548121dac7720176171bef282b2
Cameron Chamberlain <hi@camjc.com>
2013-11-11 15:38:39 +1100

    move global variables back into the color new controller

--- app/scripts/controllers/color_new_controller.js
+++ app/scripts/controllers/color_new_controller.js
@@ -6,10 +6,11 @@
 //   }
 // });
 
-var currentcolor = 'FA00E0',
-	colorname = 'unnamed',
-	currentlocation = {};
 Emotivecolor.ColorNewController = Ember.ObjectController.extend({
+	currentColor: 'FA00E0',
+	colorName: 'unnamed',
+	currentLat: 0,
+	currentLng: 0,
 	selectedEmotion: null,
 	emotions: [
 		'Happy',
@@ -27,6 +28,19 @@ Emotivecolor.ColorNewController = Ember.ObjectController.extend({
 		this.geoLocation();
 	},
 	geoLocation: function () {
+	    var _self = this;
+		function displayPosition(position) {
+			_self.setProperties({ 'currentLat': position.coords.latitude, 'currentLng': position.coords.longitude });
+		}
+		function displayError(error) {
+			var errors = { 
+				1: 'Permission denied',
+				2: 'Position unavailable',
+				3: 'Request timeout'
+			};
+			console.log('Error: ' + errors[error.code]);
+		}
+
 		if (navigator.geolocation) {
 			var timeoutVal = 10 * 1000 * 1000;
 			navigator.geolocation.getCurrentPosition(
@@ -36,23 +50,8 @@ Emotivecolor.ColorNewController = Ember.ObjectController.extend({
 				);
 		}
 		else {
-			alert('Geolocation is not supported by this browser');
+			console.log('Geolocation is not supported by this browser');
 		}
-
-		function displayPosition(position) {
-			currentlocation.lat = position.coords.latitude;
-			currentlocation.lng = position.coords.longitude;
-		}
-		function displayError(error) {
-			var errors = { 
-				1: 'Permission denied',
-				2: 'Position unavailable',
-				3: 'Request timeout'
-			};
-			alert('Error: ' + errors[error.code]);
-		}
-
-		return currentlocation;
 	},
 	controlColor: function (bgHex) {
 		var rgb = ntc.rgb('#' + bgHex),
@@ -69,36 +68,37 @@ Emotivecolor.ColorNewController = Ember.ObjectController.extend({
 	}.property('hex'),
 	getName: function() {
 		var n_match  = ntc.name('#' + this.get('hex'));
-		colorname =n_match[1];
-		return colorname;
+		this.set('colorName', n_match[1]);
+		return this.get('colorName');
 	}.property('hex'),
 	actions: {
 		generatedColor: function() {
-			currentcolor =  ('000000' + (Math.floor(Math.random()*0xFFFFFF)).toString(16)).slice(-6);
-			this.set('hex',currentcolor);
+			var genCol = ('000000' + (Math.floor(Math.random()*0xFFFFFF)).toString(16)).slice(-6);
+			this.set('currentColor', genCol);
+			this.set('hex', genCol);
 		},
 		click: function (emotion) {
 			// Create the new Color model
-			var color = this.store.createRecord('color', {
-				hex: currentcolor,
+			var thisColor = this.get('currentColor'),
+				color = this.store.createRecord('color', {
+				hex: thisColor,
 				emotion: emotion,
-				r: ntc.rgb('#' + currentcolor)[0],
-				g: ntc.rgb('#' + currentcolor)[1],
-				b: ntc.rgb('#' + currentcolor)[2],
-				h: ntc.hslCamJC('#' + currentcolor)[0],
-				s: ntc.hslCamJC('#' + currentcolor)[1],
-				l: ntc.hslCamJC('#' + currentcolor)[2],
+				r: ntc.rgb('#' + thisColor)[0],
+				g: ntc.rgb('#' + thisColor)[1],
+				b: ntc.rgb('#' + thisColor)[2],
+				h: ntc.hslCamJC('#' + thisColor)[0],
+				s: ntc.hslCamJC('#' + thisColor)[1],
+				l: ntc.hslCamJC('#' + thisColor)[2],
 				date: new Date(),
-				lat: currentlocation.lat,
-				lng: currentlocation.lng,
+				lat: this.get('currentLat'),
+				lng: this.get('currentLng'),
 			}),
-			messageColorName = colorname; // Otherwise it may have changed by the time the message appears. 
+			messageColorName = this.get('colorName'); // Otherwise it may have changed by the time the message appears. 
 
 			// Check that emotion passed from the template matches something in the above array
 			if (jQuery.inArray( emotion, this.emotions) === -1){
 				return;
 			}
-			console.log(currentlocation);
 
 			// Save the new model
 			color.save().then(function() {
