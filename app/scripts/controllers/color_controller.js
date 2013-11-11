6902e2b2315ee14d36c182806b58210725e663ee
Cameron Chamberlain <hi@camjc.com>
2013-11-11 18:53:39 +1100

    fix routing to indiv emotion

--- app/scripts/controllers/color_controller.js
+++ app/scripts/controllers/color_controller.js
@@ -10,5 +10,19 @@ Emotivecolor.ColorController = Ember.ObjectController.extend({
 		var n_match  = ntc.name('#' + this.get('hex'));
 		this.set('colorName', n_match[1]);
 		return this.get('colorName');
-	}.property('hex')
+	}.property('hex'),
+    actions: {
+        delete: function( item ){
+            // this tells Ember-Data to delete the color passed in as item
+            this.get('model').deleteRecord(item);
+            this.get('model').save().then(function() {
+                Emotivecolor.alertController.popObject(); // Removes Previous Message
+                Emotivecolor.alertController.pushObject(Ember.Object.create({ message:  "Deleted a color." }));
+                Emotivecolor.transitionToRoute('about'); //Doesn't do anything yet.
+            }, function() {
+                Emotivecolor.alertController.popObject(); // Removes Previous Message
+                Emotivecolor.alertController.pushObject(Ember.Object.create({ message:  "Could not delete this color." }));
+            });
+        }
+    }
 });
