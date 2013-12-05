7b3d5eb223a62164297e8be7ed6f6be9a514637f
Cameron Chamberlain <hi@camjc.com>
2013-12-05 16:11:21 +1100

    consolidate allcolors controller/view/route with emotion

--- app/scripts/controllers/emotion_controller.js
+++ app/scripts/controllers/emotion_controller.js
@@ -1,4 +1,25 @@
 Emotivecolor.EmotionController = Ember.ObjectController.extend({
+    currentEmotion: function () {
+        return this.get('model').get('firstObject').get('emotion');
+    }.property('@each'),
+    // avgHSL: function () {
+    //     var avgH = 0,
+    //         avgS = 0,
+    //         avgL = 0,
+    //         self = this;
+
+    //     self.get('model').forEach(function (indiv, index) {
+    //         avgH += indiv.get('h');
+    //         avgS += indiv.get('s');
+    //         avgL += indiv.get('l');
+    //     });
+
+    //     avgH = Math.round(avgH / self.get('model.length'));
+    //     avgS = Math.round(avgS / self.get('model.length'));
+    //     avgL = Math.round(avgL / self.get('model.length'));
+
+    //     return avgH + ', ' + avgS + '%, ' + avgL + '%';
+    // }.property('@each'),
     init: function () {
         var self = this;
         self.set('scene', new THREE.Scene());
@@ -6,15 +27,20 @@ Emotivecolor.EmotionController = Ember.ObjectController.extend({
         self.set('camera', new THREE.PerspectiveCamera(20, 1280 / 720, 1, 20000));
         self.set('camera.position.y', 600);
 
-        // if (window.WebGLRenderingContext) {
-            self.set('renderer', new THREE.WebGLRenderer({ antialias: true }));
-        // } else {
-        //     self.set('renderer', new THREE.CanvasRenderer());
-        // }
+        if (window.WebGLRenderingContext) {
+            self.set('renderer', new THREE.WebGLRenderer({
+                antialias: true
+            }));
+            self.set('rendererType', 'webgl');
+        } else {
+            self.set('renderer', new THREE.CanvasRenderer());
+            self.set('rendererType', 'canvas');
+        }
         self.renderer.setSize(1280, 720);
         self.renderer.setClearColor(0xD1D1D1, 1);
-        self.scene.fog = new THREE.FogExp2( 0xD1D1D1, 0.0001 );
-        self.sprite = THREE.ImageUtils.loadTexture( "images/circleB.png" );
+        self.scene.fog = new THREE.FogExp2(0xD1D1D1, 0.0001);
+
+        self.sprite = THREE.ImageUtils.loadTexture("images/circleB.png");
 
         self.set('controls', new THREE.OrbitControls(self.camera, self.renderer.domElement));
 
@@ -25,31 +51,64 @@ Emotivecolor.EmotionController = Ember.ObjectController.extend({
         document.body.appendChild(self.get('renderer').domElement);
         self.animate();
     },
-    mesher: function(){
+    program: function (context) {
+        var PI2 = Math.PI * 2;
+        context.beginPath();
+        context.arc(0, 0, 4, 0, PI2, true);
+        context.closePath();
+        context.fill();
+    },
+    mesher: function () {
         var self = this,
             geometry = new THREE.Geometry(),
-            vertexColors = [];
-        if (self.get('camera') === null){ //Initalize scene if it hasn't already (returning to view)
+            vertexColors = [],
+            material;
+        if (self.get('camera') === null) { //Initalize scene if it hasn't already (returning to view)
             self.init();
         }
-        self.get('model').forEach(function (indiv, index) {
+        if (self.get('rendererType') === 'webgl') {
+            self.get('model').forEach(function (indiv, index) {
 
-            var vertex = new THREE.Vector3();
-            vertex.x = indiv.get('radial3Y');
-            vertex.y = indiv.get('radial3Z');
-            vertex.z = indiv.get('radial3X'); //Reordered for controls to work more naturally.
-            geometry.vertices.push( vertex );
-            var thishex = indiv.get('hexo');
-            vertexColors.push(new THREE.Color( 0x0000ff ));
-            vertexColors[ index ].setHex( indiv.get('hexo') );
-        });
+                var vertex = new THREE.Vector3();
+                vertex.x = indiv.get('radial3Y');
+                vertex.y = indiv.get('radial3Z');
+                vertex.z = indiv.get('radial3X'); //Reordered for controls to work more naturally.
+                geometry.vertices.push(vertex);
+                vertexColors.push(new THREE.Color());
+                vertexColors[index].setHex(indiv.get('hexo'));
+            });
 
-        geometry.colors = vertexColors;
-        material = new THREE.ParticleSystemMaterial( { size: 50, map: self.sprite, vertexColors: true, transparent: true } );
-        material.alphaTest = 0.5; //Stops black edges on transparent png
-        self.particles = new THREE.ParticleSystem( geometry, material );
-        self.particles.sortParticles = true;
-        self.scene.add( self.particles );
+            geometry.colors = vertexColors;
+            material = new THREE.ParticleSystemMaterial({
+                size: 50,
+                map: self.sprite,
+                vertexColors: true,
+                transparent: true
+            });
+            material.alphaTest = 0.5; //Stops black edges on transparent png
+            self.particles = new THREE.ParticleSystem(geometry, material);
+            self.particles.sortParticles = true;
+
+        } else if (self.get('rendererType') === 'canvas') {
+
+            self.get('model').forEach(function (indiv, index) {
+                var indivColor = new THREE.Color();
+                indivColor.setHex(indiv.get('hexo'));
+                var material = new THREE.SpriteCanvasMaterial({
+                    color: indivColor,
+                    program: self.program
+                });
+                indiv.particle = new THREE.Particle(material);
+                indiv.particle.position.x = indiv.get('radial3Y');
+                indiv.particle.position.y = indiv.get('radial3Z');
+                indiv.particle.position.z = indiv.get('radial3X');
+
+                geometry.vertices.push(indiv.particle.position);
+                self.scene.add(indiv.particle);
+            });
+        }
+
+        self.scene.add(self.particles);
 
         return self.get('model.length');
 
@@ -62,28 +121,26 @@ Emotivecolor.EmotionController = Ember.ObjectController.extend({
     animate: function () {
         var self = this;
         if (self.camera !== null) { // Checks that the camera still exists before looping.
-            requestAnimationFrame(function() {
+            requestAnimationFrame(function () {
                 return self.animate();
             });
             self.render();
         }
     },
-    total: function () {
-        return this.get('model').reduce(function (previousValue, item, index, enumerable) {
-            return previousValue + item.h;
-        }, 0);
-        return this.get('model');
-    }.property('model.@each'),
     actions: {
         delete: function (item) {
             // this tells Ember-Data to delete the color passed in as item
             this.get('model').removeObject(item);
             this.get('model').save().then(function () {
                 Emotivecolor.alertController.popObject(); // Removes Previous Message
-                Emotivecolor.alertController.pushObject(Ember.Object.create({ message:  "Deleted a color." }));
+                Emotivecolor.alertController.pushObject(Ember.Object.create({
+                    message: "Deleted a color."
+                }));
             }, function () {
                 Emotivecolor.alertController.popObject(); // Removes Previous Message
-                Emotivecolor.alertController.pushObject(Ember.Object.create({ message:  "Could not delete this color." }));
+                Emotivecolor.alertController.pushObject(Ember.Object.create({
+                    message: "Could not delete this color."
+                }));
             });
         }
     } //Same as in the colors controller, can I DRY this?
--- app/scripts/views/emotion_view.js
+++ app/scripts/views/emotion_view.js
@@ -3,8 +3,8 @@ Emotivecolor.EmotionView = Ember.View.extend({
     willClearRender: function () {
         //remove threejs after leaving the view
         var controller = this.get('controller');
-        controller.scene.remove( controller.camera );
-        controller.scene.remove( controller.particles );
+        controller.scene.remove(controller.camera);
+        controller.scene.remove(controller.particles);
         controller.renderer.clear();
         controller.camera = null;
         controller.scene = null;
