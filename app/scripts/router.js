856e45c321fff1480a2fb20abd7fa09452f5c5ed
Cameron Chamberlain <hi@camjc.com>
2013-12-12 19:57:40 +1100

    facebook implimentation

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,7 +2,7 @@ Emotivecolor.Router.map(function () {
 
     this.resource('color', { path: '/color/:color_id' });
     this.resource('color.new', { path: '/color/new' });
-    this.resource('emotions', { path: 'emotions' });
+    this.resource('emotions');
     this.resource('emotion', { path: '/emotion/:emotion' });
     this.resource('about', { path: '/' });
     this.resource('manage');
